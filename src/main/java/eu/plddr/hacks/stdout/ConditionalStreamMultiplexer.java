534249825164fec5057a260a1ce6dd1f3fbbfb29
Patrick Lodder <patricklodder@users.noreply.github.com>
2016-12-11 12:08:59 +0100

    first iteration

--- src/main/java/eu/plddr/hacks/stdout/ConditionalStreamMultiplexer.java
+++ src/main/java/eu/plddr/hacks/stdout/ConditionalStreamMultiplexer.java
@@ -1,24 +1,71 @@
 package eu.plddr.hacks.stdout;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ConditionalStreamMultiplexer {
+public class ConditionalStreamMultiplexer extends OutputStream {
 
     private static final PrintStream originalStdOut = System.out;
 
-    private final PrintStream printStream;
+    private final List<StreamAndCondition> streamList = new ArrayList<StreamAndCondition>();
 
-    protected ConditionalStreamMultiplexer(OutputStream outputStream) {
-        this.printStream = new PrintStream(outputStream);
+    public ConditionalStreamMultiplexer(String stdOutCondition, boolean stdOutNegate) {
+        streamList.add(new StreamAndCondition(originalStdOut, stdOutCondition, stdOutNegate));
     }
 
-    public void assign() {
-        System.setOut(printStream);
+    public void register() {
+        System.setOut(new PrintStream(this));
+    }
+
+    public void unregister() {
+        System.setOut(originalStdOut);
+    }
+
+    public void addConditionalStream(OutputStream stream, String condition, boolean negate) {
+        streamList.add(new StreamAndCondition(stream, condition, negate));
     }
 
     protected OutputStream getOriginalStdOut() {
         return originalStdOut;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        for (StreamAndCondition sac : streamList) {
+            if (sac.matchesInt())
+                sac.getStream().write(b);
+        }
+    }
+
+    @Override
+    public void write(byte b[]) throws IOException {
+        for (StreamAndCondition sac : streamList) {
+            if (sac.matchesInput(b))
+                sac.getStream().write(b);
+        }
+    }
+
+    @Override
+    public void write(byte b[], int off, int len) throws IOException {
+        byte[] cmp = new byte[len];
+
+        if (b == null) {
+            throw new NullPointerException();
+        } else if ((off < 0) || (off > b.length) || (len < 0) ||
+                ((off + len) > b.length) || ((off + len) < 0)) {
+            throw new IndexOutOfBoundsException();
+        } else if (len == 0) {
+            return;
+        }
+
+        for (int i = 0 ; i < len ; i++) {
+            cmp[i] = b[i+off];
+        }
+
+        write(cmp);
     }
 
 }
--- src/main/java/eu/plddr/hacks/stdout/StreamAndCondition.java
+++ src/main/java/eu/plddr/hacks/stdout/StreamAndCondition.java
@@ -1,7 +1,32 @@
 package eu.plddr.hacks.stdout;
 
-/**
- * Created by patrick on 11/12/16.
- */
+import java.io.OutputStream;
+
 public class StreamAndCondition {
+
+    private final OutputStream stream;
+    private final String condition;
+    private final boolean negate;
+
+    public StreamAndCondition(OutputStream stream, String condition, boolean negate) {
+        this.stream = stream;
+        this.condition = condition;
+        this.negate = negate;
+    }
+
+    public boolean matchesInput(byte[] data) {
+        if (condition == null)
+            return !negate;
+
+        return (data.length != 0) && ((new String(data)).matches(condition)) ^ negate;
+    }
+
+    public boolean matchesInt() {
+        return condition != null || !negate;
+
+    }
+
+    public OutputStream getStream() {
+        return stream;
+    }
 }
